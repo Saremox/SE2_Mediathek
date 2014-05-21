0e2450032dbe7093872035e220bba765fa53d0aa
Michael Strassberger <3strassb@informatik.uni-hamburg.de>
2014-05-21 13:09:07 +0200

    Implemented View for vorgemerkt von

--- src/de/uni_hamburg/informatik/swt/se2/mediathek/werkzeuge/subwerkzeuge/ausleihemedienauflister/AusleiheMedienauflisterWerkzeug.java
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/werkzeuge/subwerkzeuge/ausleihemedienauflister/AusleiheMedienauflisterWerkzeug.java
@@ -85,7 +85,7 @@ public class AusleiheMedienauflisterWerkzeug extends ObservableSubWerkzeug
             // Ist dies korrekt implementiert, erscheint in der Ausleiheansicht
             // der Name des Vormerkers, an den ein Medium ausgeliehen werden
             // darf, gemäß Anforderung d).
-            Kunde ersterVormerker = null;
+            Kunde ersterVormerker = _verleihService.getVormerker(0, medium);
 
             medienFormatierer.add(new AusleiheMedienFormatierer(medium,
                     istVerliehen, ersterVormerker));
